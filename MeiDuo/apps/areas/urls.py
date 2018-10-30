1a7520f9d50da761ca14a39100a4adbf5d66a6c7
bigfoolliu <liu15671677014@icloud.com>
2018-10-30 13:18:43 +0800

    视图集实现查询收货地址

--- MeiDuo/apps/areas/urls.py
+++ MeiDuo/apps/areas/urls.py
@@ -5,13 +5,17 @@
 
 
 from django.urls import path, re_path
+from rest_framework.routers import DefaultRouter
+
 from .views import *
 
 
 urlpatterns = [
-    path('areas/', AreaListView.as_view()),
-    re_path(r'areas/(?P<pk>\d+)/', AreaRetrieveView.as_view()),
+    # path('areas/', AreaListView.as_view()),
+    # re_path(r'areas/(?P<pk>\d+)/', AreaRetrieveView.as_view()),
 ]
 
 # 用视图集来代替上方的两个url
-
+router = DefaultRouter()
+router.register('areas', AreaViewSet, base_name='areas')
+urlpatterns += router.urls
--- MeiDuo/apps/areas/views.py
+++ MeiDuo/apps/areas/views.py
@@ -12,21 +12,50 @@ from areas.models import Area
 from areas.serializers import AreaSerializer, AreaSubSerializer
 
 
-class AreaListView(ListAPIView):
-    """
-    查询省份列表视图接口
-    """
-    queryset = Area.objects.filter(parent__isnull=True)  # 省份的父级id为空
-    serializer_class = AreaSerializer
-    # get方法
+# class AreaListView(ListAPIView):
+#     """
+#     查询省份列表视图接口
+#     """
+#     queryset = Area.objects.filter(parent__isnull=True)  # 省份的父级id为空
+#     serializer_class = AreaSerializer
+#     # get方法
+#
+#
+# class AreaRetrieveView(RetrieveAPIView):
+#     """
+#     查询下辖区域列表视图接口
+#     """
+#     queryset = Area.objects.all()
+#     serializer_class = AreaSubSerializer
+#     # get方法
 
 
-class AreaRetrieveView(RetrieveAPIView):
+class AreaViewSet(ReadOnlyModelViewSet):
     """
-    查询下辖区域列表视图接口
+    视图集来合并上面的两个View
+    这里不能直接指定queryset和serializer_class,因为上述查询的方式和序列化器均不一样
+    利用action来解决,即指定请求方式与处理函数的对应关系
     """
-    queryset = Area.objects.all()
-    serializer_class = AreaSubSerializer
-    # get方法
+
+    def get_queryset(self):
+        """
+        指定查询集
+        :return:
+        """
+        # 如果查询的是省份,返回父类id为空的所有对象
+        if self.action == 'list':
+            return Area.objects.filter(parent__isnull=True)
+        else:
+            return Area.objects.all()
+
+    def get_serializer_class(self):
+        """
+        指定序列化器
+        :return:
+        """
+        if self.action == 'list':
+            return AreaSerializer
+        else:
+            return AreaSubSerializer
 
 
