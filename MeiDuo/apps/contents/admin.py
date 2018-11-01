18881cad89ce54ea17168cdedcfe8b59c86efdb5
bigfoolliu <liu15671677014@icloud.com>
2018-11-01 21:40:16 +0800

    将模型类注册到后台(未完成添加FastDFS保存的测试图片数据)

--- MeiDuo/apps/contents/admin.py
+++ MeiDuo/apps/contents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+"""
+在后台管理中注册模型类
+"""
+admin.site.register(ContentCategory)
+admin.site.register(Content)
--- MeiDuo/apps/goods/admin.py
+++ MeiDuo/apps/goods/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(GoodsCategory)
+admin.site.register(GoodsChannel)
+admin.site.register(Goods)
+admin.site.register(Brand)
+admin.site.register(GoodsSpecification)
+admin.site.register(SpecificationOption)
+admin.site.register(SKU)
+admin.site.register(SKUSpecification)
+admin.site.register(SKUImage)
