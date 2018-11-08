20636b403fd5a555f9ea9134bc8b5f020f6db36f
xwp <xwp_fullstack@163.com>
2018-11-08 10:33:16 +0800

    更改配置,使存入myql的时间均为本地时间

--- MeiDuo/settings/dev.py
+++ MeiDuo/settings/dev.py
@@ -141,7 +141,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 更改该项配置，从而时间存入到mysql时使用的时本地之间而不是UTC时间
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
